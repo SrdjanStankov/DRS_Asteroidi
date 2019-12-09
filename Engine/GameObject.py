3e0aebee7fff33e653aeb8cde6d8295cde699f01
Srdjan Stankov <srki.miki@gmail.com>
2019-12-09 21:35:39 +0100

    Added Transform component to GameObject

--- Engine/GameObject.py
+++ Engine/GameObject.py
@@ -1,9 +1,10 @@
 import GameLoop as gl
-
+import Transform
 
 class GameObject():
 
     def __init__(self, *args, **kwargs):
         gl.GameLoop.getInstance().connect_to_update(self.update)
+        self.transform = Transform.Transform()
     def update(self):
         print("GAME OBJECT UPDATE")
