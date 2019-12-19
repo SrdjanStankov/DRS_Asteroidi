e236cfca38fe09522253fbd39b972619abb1ff6f
Srdjan Stankov <srki.miki@gmail.com>
2019-12-19 16:02:16 +0100

    created test for running game loop from main game

--- Game/MainGame.py
+++ Game/MainGame.py
@@ -1,1 +1,22 @@
+from GameObject import GameObject
 
+class Asteroid(GameObject):
+    
+    def __init__(self, *args, **kwargs):
+        return super().__init__(*args, **kwargs)
+
+    def update(self):
+        print("asteroid update")
+
+
+if __name__ == "__main__":
+    
+    a = Asteroid();
+
+    from time import sleep
+
+    sleep(5);
+
+    from GameLoop import GameLoop as gl
+
+    gl.getInstance().cancel();
