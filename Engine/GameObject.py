bf4f94c7f5d467e03202ea49095fc5235d640730
Srdjan Stankov <srki.miki@gmail.com>
2019-12-19 16:28:30 +0100

    Added documentation for GameLoop and GameObject

--- Engine/GameObject.py
+++ Engine/GameObject.py
@@ -4,7 +4,11 @@ import Transform
 class GameObject():
 
     def __init__(self, *args, **kwargs):
+        """If you are defining something inside here be sure to call __init__ of base class"""
         gl.GameLoop.getInstance().connect_to_update(self.update)
         self.transform = Transform.Transform()
+    
+    # Nesto se desava
     def update(self):
+        """This method will be automatically called inside Game Loop when defined."""
         pass
