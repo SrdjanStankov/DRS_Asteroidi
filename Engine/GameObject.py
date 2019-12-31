d16979714806cba298fc523d3ca04c565d74a093
RusCheDell <acarista1@live.com>
2019-12-31 11:15:43 +0100

    Priprema za kolizije

--- Engine/GameObject.py
+++ Engine/GameObject.py
@@ -8,6 +8,7 @@ class GameObject():
         gl.GameLoop.getInstance().connect_to_update(self.update)
         self.transform = Transform.Transform()
         self.name = ""
+        self.collisionWithId = -1
     
     # Nesto se desava
     def update(self):
