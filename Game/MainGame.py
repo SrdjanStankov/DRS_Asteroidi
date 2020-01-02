51b39d3502d14ad0a64148c21783165afeb6e8f8
Srdjan Stankov <srki.miki@gmail.com>
2020-01-02 14:56:38 +0100

    Moved Player logic to new file

--- Game/MainGame.py
+++ Game/MainGame.py
@@ -1,46 +1,11 @@
 import sys
-from Asteroid import Asteroid
 from Player import Player
 from PyQt5 import QtWidgets
 from GameLoop import GameLoop as gl
 
-import InputCommandType as inputCommand
-import GameObject as gameObject
-
 import Managers as mgr
 import AsteroidManager as AsteroidManager
-import Vector as vector
-import Transform as transform
 import ProjectileManager as ProjectileManager
-from AsteroidManager import AsteroidBeh
-# Example
-class SimpleGO(gameObject.GameObject):
-    def __init__(self,projectileManager):
-        super().__init__()
-        self.go1 = mgr.Managers.getInstance().objects.Instantiate("Spaceship")
-        self.go1.transform.speed = 2
-        self.go1.transform.x = 1000
-        self.go1.transform.y = 450
-        self.shootCounter = 0
-        self.projectiles = []
-        self.projectileManager = projectileManager
-        
-    def update(self):
-        self.shootCounter = (self.shootCounter + 1) % 50
-        command = mgr.Managers.getInstance().input.GetCommand() 
-        if  command == inputCommand.InputCommandType.left:
-            self.go1.transform.rotate(-1)
-        if command == inputCommand.InputCommandType.right:
-            self.go1.transform.rotate(1)
-        if command == inputCommand.InputCommandType.up:
-            self.go1.transform.move(1)
-        if command == inputCommand.InputCommandType.down:
-            self.go1.transform.move(-1)
-        if command == inputCommand.InputCommandType.shoot:
-            if self.shootCounter == 0:
-                self.projectileManager.createProjectile(self.go1)        
-            
-
 
 # method for canceling game loop thread
 def cancel():
@@ -58,7 +23,7 @@ if __name__ == "__main__":
     sceneManager.show()
     objectManager = mgr.Managers.getInstance().objects
     projectileManager = ProjectileManager.ProjectileManager()
-    go = SimpleGO(projectileManager)
+    go = Player(projectileManager)
     asteroidManager = AsteroidManager.AsteroidManager()
     for i in range(1,30):
         asteroidManager.createAsteroid(100 + 50 * i,0,5)
--- Game/Player.py
+++ Game/Player.py
@@ -1,9 +1,30 @@
 from GameObject import GameObject
+import Managers as mgr
+import InputCommandType as inputCommand
 
 class Player(GameObject):
     
-    def __init__(self, *args, **kwargs):
-        return super().__init__(*args, **kwargs)
-
+    def __init__(self,projectileManager):
+        super().__init__()
+        self.go1 = mgr.Managers.getInstance().objects.Instantiate("Spaceship")
+        self.go1.transform.speed = 2
+        self.go1.transform.x = 1000
+        self.go1.transform.y = 450
+        self.shootCounter = 0
+        self.projectiles = []
+        self.projectileManager = projectileManager
+        
     def update(self):
-        print("Player update");
+        self.shootCounter = (self.shootCounter + 1) % 50
+        command = mgr.Managers.getInstance().input.GetCommand() 
+        if  command == inputCommand.InputCommandType.left:
+            self.go1.transform.rotate(-1)
+        if command == inputCommand.InputCommandType.right:
+            self.go1.transform.rotate(1)
+        if command == inputCommand.InputCommandType.up:
+            self.go1.transform.move(1)
+        if command == inputCommand.InputCommandType.down:
+            self.go1.transform.move(-1)
+        if command == inputCommand.InputCommandType.shoot:
+            if self.shootCounter == 0:
+                self.projectileManager.createProjectile(self.go1)
