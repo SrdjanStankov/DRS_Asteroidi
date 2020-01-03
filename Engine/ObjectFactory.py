7258ad6fe2a8248d3ccc84c3143d10881125bfb8
Srdjan Stankov <srki.miki@gmail.com>
2020-01-03 15:39:31 +0100

    another one

--- Engine/ObjectFactory.py
+++ Engine/ObjectFactory.py
@@ -12,7 +12,7 @@ import typing
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import QPointF, QThread, pyqtSignal, QRectF
 from PyQt5.QtGui import QBrush, QColor, QPen, QPainterPath, QPixmap
-from PyQt5.Qt import Qt
+#from PyQt5.Qt import Qt
 from PyQt5.QtWidgets import QWidget, QStyleOptionGraphicsItem, QGraphicsItem
 
 
