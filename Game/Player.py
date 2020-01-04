33e851f6f5e5b4c8c4cd32c600e3b8325a5a518d
Srdjan Stankov <srki.miki@gmail.com>
2020-01-04 17:35:40 +0100

    Dodata kolizija broda sa asteroidom posle koje se brod unisti

--- Game/Player.py
+++ Game/Player.py
@@ -7,10 +7,10 @@ class Player(GameObject):
 
     def __init__(self,name,projectileManager):
         super().__init__()
-        self.go1 = mgr.Managers.getInstance().objects.Instantiate("Spaceship",name = name)
-        self.go1.transform.speed = 2
-        self.go1.transform.x = 1000
-        self.go1.transform.y = 450
+        self.spaceshipObj = mgr.Managers.getInstance().objects.Instantiate("Spaceship",name = name)
+        self.spaceshipObj.transform.speed = 2
+        self.spaceshipObj.transform.x = 1000
+        self.spaceshipObj.transform.y = 450
         self.shootCounter = 0
         self.projectiles = []
         self.projectileManager = projectileManager
@@ -18,15 +18,29 @@ class Player(GameObject):
         self.nextShootTime = time.time()
 
     def update(self):
+
+        if self.isDeath():
+            return
+
         command = mgr.Managers.getInstance().input.GetCommand() 
         if command == inputCommand.InputCommandType.left:
-            self.go1.transform.rotate(-1)
+            self.spaceshipObj.transform.rotate(-1)
         if command == inputCommand.InputCommandType.right:
-            self.go1.transform.rotate(1)
+            self.spaceshipObj.transform.rotate(1)
         if command == inputCommand.InputCommandType.up:
-            self.go1.transform.move(1)
+            self.spaceshipObj.transform.move(1)
         if command == inputCommand.InputCommandType.down:
-            self.go1.transform.move(-1)
+            self.spaceshipObj.transform.move(-1)
         if command == inputCommand.InputCommandType.shoot and time.time() > self.nextShootTime:
-            self.projectileManager.createProjectile(self.go1)
+            self.projectileManager.createProjectile(self.spaceshipObj)
             self.nextShootTime = time.time() + self.shootInterval 
+
+    def isDeath(self):
+        for item in self.spaceshipObj.collisionsType:
+            if item == "Asteroid":
+                try:
+                    mgr.Managers.getInstance().objects.Destroy(self.spaceshipObj.Id)
+                    return True
+                except:
+                    pass
+        return False
