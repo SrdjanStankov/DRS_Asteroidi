a8fa4d0811be34c985685758f1d54dc388cb8ad4
Nemanja Kovacevic <nemanjakovacevic1997@gmail.com>
2020-01-09 22:00:07 +0100

    Updated ItemFactory for SpeedUp, ExtraLife and FireRateSpeedUp

--- Engine/ItemFactory.py
+++ Engine/ItemFactory.py
@@ -9,6 +9,13 @@ class ItemFactory():
         self.asteroidPaths = {}
         self.playerImages = {}
         self.playerPaths = {}
+        self.speedUpImage = None
+        self.speedUpPath = None
+        self.fireRateSpeedUpImage = None
+        self.fireRateSpeedUpPath = None
+        self.extraLifeImage = None
+        self.extraLifePath = None
+
         if "largeAsteroidWidth" in kwargs and "largeAsteroidHeight" in kwargs: 
             self.asteroidImages[AsteroidType.large], self.asteroidPaths[AsteroidType.large] = self.makeAsteroid(kwargs["largeAsteroidWidth"],kwargs["largeAsteroidHeight"],'asteroid1.png')
         if "player1Height" in kwargs and "player1Width" in kwargs:
@@ -17,7 +24,13 @@ class ItemFactory():
             self.asteroidImages[AsteroidType.medium], self.asteroidPaths[AsteroidType.medium] = self.makeAsteroid(kwargs["mediumAsteroidWidth"],kwargs["mediumAsteroidHeight"],'asteroid1.png')
         if "smallAsteroidWidth" in kwargs and "smallAsteroidHeight" in kwargs: 
             self.asteroidImages[AsteroidType.small], self.asteroidPaths[AsteroidType.small] = self.makeAsteroid(kwargs["smallAsteroidWidth"],kwargs["smallAsteroidHeight"],'asteroid1.png')
-        
+        if "speedUpWidth" in kwargs and "speedUpHeight" in kwargs:
+            self.speedUpImage, self.speedUpPath = self.make_SpeedUp_FireRateSpeedUp_ExtraLife(kwargs["speedUpWidth"], kwargs["speedUpHeight"], 'flash.png')
+        if "fireRateSpeedUpWidth" in kwargs and "fireRateSpeedUpHeight" in kwargs:
+            self.fireRateSpeedUpImage, self.fireRateSpeedUpPath = self.make_SpeedUp_FireRateSpeedUp_ExtraLife(kwargs["fireRateSpeedUpWidth"], kwargs["fireRateSpeedUpHeight"], 'fire.png')
+        if "extraLifeWidth" in kwargs and "extraLifeHeight" in kwargs:
+            self.extraLifeImage, self.extraLifePath = self.make_SpeedUp_FireRateSpeedUp_ExtraLife(kwargs["extraLifeWidth"], kwargs["extraLifeHeight"], 'hart.jpg')
+
 
     def getPlayer(self,playerType : PlayerType):
         if playerType == PlayerType.player1:
@@ -33,6 +46,18 @@ class ItemFactory():
                 return None, None
         else:
             return None, None
+
+    def getSpeedUp(self):
+        if self.speedUpImage is not None and self.speedUpPath is not None:
+            return self.speedUpImage, self.speedUpPath
+
+    def getFireRateSpeedUp(self):
+        if self.fireRateSpeedUpImage is not None and self.fireRateSpeedUpPath is not None:
+            return self.fireRateSpeedUpImage, self.fireRateSpeedUpImage
+
+    def getExtraLife(self):
+        if self.extraLifeImage is not None and self.extraLifePath is not None:
+            return self.extraLifeImage, self.extraLifePath
 
     def makeAsteroid(self,width,height,imageName):
         img3 = QtGui.QImage(imageName)
@@ -69,3 +94,20 @@ class ItemFactory():
         spaceshipPath.addPolygon(polygonSpaceship)
         spaceshipPath.closeSubpath()
         return imageSpaceship, spaceshipPath
+
+    def make_SpeedUp_FireRateSpeedUp_ExtraLife(self, width, height, image):
+        img1 = QtGui.QImage(image)
+        img2 = img1.scaledToHeight(width)
+        imagee = img2.scaledToWidth(height)
+        imagee.convertToFormat(QtGui.QImage.Format_ARGB32)
+        polygon = QtGui.QPolygonF([
+            QPointF(0, 0),
+            QPointF(0, height),
+            QPointF(width, height),
+            QPointF(width, 0)
+        ])
+        path = QtGui.QPainterPath()
+        path.addPolygon(polygon)
+        path.closeSubpath()
+
+        return imagee, path
